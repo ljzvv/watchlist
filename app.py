3ee677830835775b10a40f4215d3114bc1d81a50
LJ <jz365@qq.com>
2023-05-23 09:04:44 +0800

    Add base template and error template

--- app.py
+++ app.py
@@ -61,8 +61,18 @@ def forge():
     db.session.commit()
     click.echo('Done.')
 
+@app.context_processor
+def inject_user():  # 函数名可以随意修改
+    user = User.query.first() # 创建的任意一个模板，都可以在模板中直接使用 user 变量
+    return dict(user=user)  # 需要返回字典，等同于 return {'user': user}
+       
+@app.errorhandler(404)  # 传入要处理的错误代码
+def page_not_found(e):  # 接受异常对象作为参数
+    return render_template('404.html'), 404  # 返回模板和状态码
+    
 @app.route('/')
 def index():
-    user = User.query.first()  # 读取用户记录
     movies = Movie.query.all()  # 读取所有电影记录
-    return render_template('index.html', user=user, movies=movies)
+    return render_template('index.html', movies=movies)
+    
+
